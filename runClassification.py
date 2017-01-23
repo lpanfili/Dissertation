9fef0f6d7f03619b4cfa6360f821711b9d967043
lpanfili <lpanfili@uw.edu>
2017-01-23 09:18:43 -0800

    Renamed cleanFeatures to runClassification, added grid search

--- runClassification.py
+++ runClassification.py
@@ -1,10 +1,14 @@
 import csv
 import random
 from sklearn import svm, metrics
-from sklearn.model_selection import cross_val_predict
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_predict, StratifiedShuffleSplit, GridSearchCV
+from matplotlib.colors import Normalize
 import matplotlib.pyplot as plt
 import numpy as np
 import itertools
+import argparse
+import statistics
 
 # Make a list from a file of words
 # Use this for the stop words to be excluded
@@ -16,28 +20,13 @@ def makeList(filename):
 			stopWords.append(line)
 	return stopWords
 
-# Removes tokens labeled as "0" or "1"
-# Removes tokens belonging to stop words
-# Rewrites into results file
-def remove(filename, results, stopList):
-	stopWords = makeList(stopList)
-	with open(filename) as f:
-		with open(results, "w") as rf:
-			reader = csv.DictReader(f, delimiter = "\t")
-			writer = csv.DictWriter(rf, reader.fieldnames)
-			writer.writeheader()
-			for line in reader:
-				if line["phonation"] != "0" and line["phonation"] != "1" and line["word_label"] not in stopWords:
-					writer.writerow(line)
-
 # Picks which of the features to include
 # Replaces "undefined" with "1"
 # Returns a tuple of features, label
 def pickFeatures(line):
 	label = line["phonation"]
-	features = [line["jitter_ddp"], line["jitter_loc"], line["jitter_loc_abs"], line["jitter_rap"], line["jitter_ppq5"], 
-			line["shimmer_loc"], line["shimmer_apq3"], line["shimmer_apq5"], line["shimmer_apq11"], line["shimmer_dda"]]
-	features = [1 if i == "--undefined--" else i for i in features]
+	features = [line["gridfile"], line["jitter_ddp"], line["jitter_loc"], line["jitter_loc_abs"], line["jitter_rap"], line["jitter_ppq5"], 
+			line["shimmer_loc"], line["shimmer_apq3"], line["shimmer_apq5"], line["shimmer_apq11"], line["F0"]]
 	return features, label
 
 # Returns confusion matrix
@@ -80,26 +69,62 @@ def getData(filename, stopWords):
 	dataPoints = []
 	phonationLabs = ["B", "C", "M"]
 	with open(filename) as f:
-		reader = csv.DictReader(f, delimiter = "\t")
+		reader = csv.DictReader(f)
 		for line in reader:
 			if line["phonation"] in phonationLabs and line["word_label"] not in stopWords:
 				dataPoints.append(pickFeatures(line))
 	return dataPoints
 
-def printAccuracy(testy, predictedy):
-	numCorrect = 0
-	numTotal = 0
-	for i in range(len(predictedy)):
-		numTotal += 1
-		if predictedy[i] == testy[i]:
-			numCorrect += 1
-	print(numCorrect, " / ", numTotal, " = ", 1.0 * numCorrect/numTotal)
+# Inputs should be path for stop list and path for data
+def parseArgs():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("stoplist")
+	parser.add_argument("data")
+	return parser.parse_args()
+
+# take the data that will go in the model plus the speaker id
+# znormalize all features within speaker
+# go through all the data and make speaker specific lists
+# key = speaker; value = list of tuple of features + label
+def zNorm(data):
+	bySpeaker = {}
+	for i in data:
+		if i[0][0] not in bySpeaker:
+			bySpeaker[i[0][0]] = []
+		bySpeaker[i[0][0]].append(i)
+	for datapoints in bySpeaker.values():
+		numColumns = len(datapoints[0][0])
+		for i in range(1,numColumns):
+			featureVals = []
+			for datapoint in datapoints:
+				if datapoint[0][i] != "--undefined--":
+					featureVals.append(float(datapoint[0][i]))
+			featureMean = statistics.mean(featureVals)
+			featureSTD = statistics.stdev(featureVals)
+			for datapoint in datapoints:
+				if datapoint[0][i] != "--undefined--":
+					datapoint[0][i] = (float(datapoint[0][i]) - featureMean) / featureSTD
+				else:
+					datapoint[0][i] = 1
+	# Get rid of speaker IDs
+	for datapoint in data:
+		datapoint[0].pop(0)
+
+	print(data)
 
 def main():
-	stopWords = makeList("/Users/Laura/Desktop/Dissertation/test-data/phonetic_stoplist.txt")
-	dataPoints = getData("/Users/Laura/Desktop/Dissertation/test-data/results.txt", stopWords)
+	args = parseArgs()
+	stopWords = makeList(args.stoplist)
+	dataPoints = getData(args.data, stopWords)
+	zNorm(dataPoints)
+	# Remove speaker name
+	#stopWords = makeList("/Users/Laura/Desktop/Dissertation/test-data/phonetic_stoplist.txt")
+	#dataPoints = getData("/Users/Laura/Desktop/Dissertation/test-data/results.txt", stopWords)
 	random.shuffle(dataPoints)
 	trainx, trainy = zip(*dataPoints)
+
+	#clf = svm.SVC(kernel = 'rbf', C = .1, gamma = 0.001)
+	#clf = svm.SVC(kernel = 'rbf')
 	clf = svm.SVC()
 	predictedy = cross_val_predict(clf, trainx, trainy, cv=10)
 	print(metrics.accuracy_score(trainy, predictedy))
@@ -107,6 +132,16 @@ def main():
 	plt.figure()
 	plot_confusion_matrix(cnf_matrix, classes = ["B", "M", "C"], title = 'Confusion Matrix')
 	plt.show()
-
+"""
+# Grid search
+	C_range = np.logspace(-3, 2, 6)
+	gamma_range = np.logspace(-3, 2, 6)
+	param_grid = dict(gamma=gamma_range, C=C_range)
+	cv = StratifiedShuffleSplit(n_splits=10, test_size=0.1, random_state=42)
+	grid = GridSearchCV(clf, param_grid=param_grid, cv=cv)
+	grid.fit(trainx, trainy)
+	print("The best parameters are %s with a score of %0.2f"
+      % (grid.best_params_, grid.best_score_))
+"""
 if __name__ == "__main__":
 	main()
