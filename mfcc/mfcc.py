d819568dcdabf1bc3317d2d0ee98c5fdced97b98
lpanfili <lpanfili@uw.edu>
2017-04-23 14:14:45 -0700

    Separates by vowel.

--- mfcc/mfcc.py
+++ mfcc/mfcc.py
@@ -30,9 +30,12 @@ def cleanData(stopWords):
 		reader = csv.reader(f, delimiter = '\t')
 		header = next(reader)
 		for line in reader:
-			if line[6] != "0" and line[6] != "1":
+			if line[6] == "B" or line[6] == "M" or line[6] == "C":
 				if line[5] not in stopWords:
-					newline = [line[6], float(line[7]), float(line[8]), float(line[9]), 
+					vowel = line[4]
+					if ord(vowel[-1]) >=48 and ord(vowel[-1]) <=50:
+						vowel = vowel[:-1]
+					newline = [line[6], vowel, float(line[7]), float(line[8]), float(line[9]), 
 					float(line[10]), float(line[11]), float(line[12]), 
 					float(line[13]), float(line[14]), float(line[15]), float(line[16]), 
 					float(line[17]), float(line[18]), float(line[19]), float(line[20]),
@@ -42,30 +45,30 @@ def cleanData(stopWords):
 					newdata.append(newline)
 	return newdata
 
+
 def makeDict(data):
-	for i in range(1,25):
-		B = []
-		M = []
-		C = []
-		for row in data:
-			if row[0] == "B":
-				B.append(row[i])
-			if row[0] == "M":
-				M.append(row[i])
-			if row[0] == "C":
-				C.append(row[i])
-		plot(B, M, C, i)
+	mfccDict = {}
+	for row in data:
+		vowel = row[1]
+		if vowel not in mfccDict:
+			mfccDict[vowel] = {}
+			for i in range(2,26):
+				mfccDict[vowel][i] = {"B":[], "M":[], "C":[]}
+		for i in range(2,26):
+			mfccDict[vowel][i][row[0]].append(row[i])
+	for vowel in mfccDict:
+		for	cc in mfccDict[vowel]:
+			plot(mfccDict[vowel][cc]["B"], mfccDict[vowel][cc]["M"], mfccDict[vowel][cc]["C"], cc, vowel)
 
 
-def plot(B, M, C, i):
+def plot(B, M, C, i, vowel):
 	toPlot = [B, M, C]
 	plt.boxplot(toPlot, labels = ["B", "M", "C"])
-	number = "MFCC " + str(i)
+	number = "MFCC " + str(i - 1) + ", " + vowel
 	plt.title(number)
 	#plt.show()
-	plt.savefig(''.join(["/Users/Laura/Desktop/Dissertation/Code/mfcc/mfcc-means/", str(i)]), dpi = "figure")
+	plt.savefig(''.join(["/Users/Laura/Desktop/Dissertation/Code/mfcc/mfcc-means/", str(i - 1), vowel]), dpi = "figure")
 	plt.clf()
-
 
 def main():
 	setFont()
