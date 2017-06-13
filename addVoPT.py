d97e1cb12908b0872020edaf87c8bb8d1f2a737c
lpanfili <lpanfili@uw.edu>
2017-06-13 09:10:54 -0700

    First commit.

---
+++ addVoPT.py
@@ -0,0 +1,29 @@
+# Given an almost-complete data set, calculates and adds VoPT
+# Also prints preceding and surrounding phones so I have the list
+
+import csv
+import numpy as np
+import itertools
+
+w = csv.writer(open("/Users/Laura/Desktop/Dissertation/data/gujarati/guj-all.csv", "w"))
+with open("/Users/Laura/Desktop/Dissertation/data/gujarati/guj-all0.csv") as f:
+	reader = csv.reader(f)
+	header = next(reader)
+	newheader = header
+	newheader.insert(119, "VoPT")
+	w.writerow(newheader)
+	for line in reader:
+		VoPT = 0
+		strF0 = line[103]
+		sF0 = line[107]
+		pF0 = line[111]
+		shrF0 = line[115]
+		tracks = [strF0, sF0, pF0, shrF0]
+		pairs = (list(itertools.combinations(tracks, 2)))
+		for pair in pairs:
+			a = float(pair[0])
+			b = float(pair[1])
+			diff = abs(a - b)
+			VoPT += diff
+		line.insert(119, VoPT)
+		w.writerow(line)
