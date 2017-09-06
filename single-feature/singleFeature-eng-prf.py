8911798de6e331cfa14ce86c452bac554b23829d
lpanfili <lpanfili@uw.edu>
2017-09-05 18:51:34 -0700

    Calculations made cumulatively over folds, not averaged over folds.

--- single-feature/singleFeature-eng-prf.py
+++ single-feature/singleFeature-eng-prf.py
@@ -9,19 +9,18 @@ import numpy as np
 import sklearn
 from sklearn import svm, metrics
 from sklearn.svm import SVC
-from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score, \
+    confusion_matrix, classification_report
 from sklearn.model_selection import StratifiedKFold, cross_val_predict
 import argparse
 from collections import Counter
 
-# This list will contain the output for each single feature model
-accuracy = [] 
 # This is a list of all the features to test (their header names)
 #features = ['H1A1c_mean','H1A1c_means001','H1A1c_means002','H1A1c_means003','H1A2c_mean','H1A2c_means001','H1A2c_means002','H1A2c_means003','H1A3c_mean','H1A3c_means001','H1A3c_means002','H1A3c_means003','H1H2c_mean','H1H2c_means001','H1H2c_means002','H1H2c_means003','H2H4c_mean','H2H4c_means001','H2H4c_means002','H2H4c_means003','H42Kc_mean','H42Kc_means001','H42Kc_means002','H42Kc_means003','H2KH5Kc_mean','H2KH5Kc_means001','H2KH5Kc_means002','H2KH5Kc_means003','jitter_loc_mean','jitter_loc_1','jitter_loc_2','jitter_loc_3','jitter_loc_abs_mean','jitter_loc_abs_1','jitter_loc_abs_2','jitter_loc_abs_3','jitter_rap_mean','jitter_rap_1','jitter_rap_2','jitter_rap_3','jitter_ppq5_mean','jitter_ppq5_1','jitter_ppq5_2','jitter_ppq5_3','Energy_mean','Energy_means001','Energy_means002','Energy_means003','shimmer_loc_mean','shimmer_loc_1','shimmer_loc_2','shimmer_loc_3','shimmer_local_dB_mean','shimmer_loc_db_1','shimmer_loc_db_2','shimmer_loc_db_3','shimmer_apq3_mean','shimmer_apq3_1','shimmer_apq3_2','shimmer_apq3_3','shimmer_apq5_mean','shimmer_apq5_1','shimmer_apq5_2','shimmer_apq5_3','shimmer_apq11_mean','shimmer_apq11_1','shimmer_apq11_2','shimmer_apq11_3','HNR05_mean','HNR05_means001','HNR05_means002','HNR05_means003','HNR15_mean','HNR15_means001','HNR15_means002','HNR15_means003','HNR25_mean','HNR25_means001','HNR25_means002','HNR25_means003','HNR35_mean','HNR35_means001','HNR35_means002','HNR35_means003','SHR_mean','SHR_means001','SHR_means002','SHR_means003','CPP_mean','CPP_means001','CPP_means002','CPP_means003','pF0_mean','pF0_means001','pF0_means002','pF0_means003','shrF0_mean','shrF0_means001','shrF0_means002','shrF0_means003','sF0_mean','sF0_means001','sF0_means002','sF0_means003','strF0_mean','strF0_means001','strF0_means002','strF0_means003','VoPT','sF1_mean','sF1_means001','sF1_means002','sF1_means003','vowel_dur']
-#features = ['H1A1c_means001']
 # ENGLISH ONLY FEATURES
-features = ['H1A1c_mean','H1A1c_means001','H1A1c_means002','H1A1c_means003','H1A2c_mean','H1A2c_means001','H1A2c_means002','H1A2c_means003','H1A3c_mean','H1A3c_means001','H1A3c_means002','H1A3c_means003','H1H2c_mean','H1H2c_means001','H1H2c_means002','H1H2c_means003','H2H4c_mean','H2H4c_means001','H2H4c_means002','H2H4c_means003','H42Kc_mean','H42Kc_means001','H42Kc_means002','H42Kc_means003','H2KH5Kc_mean','H2KH5Kc_means001','H2KH5Kc_means002','H2KH5Kc_means003','jitter_loc_mean','jitter_loc_1','jitter_loc_2','jitter_loc_3','jitter_loc_abs_mean','jitter_loc_abs_1','jitter_loc_abs_2','jitter_loc_abs_3','jitter_rap_mean','jitter_rap_1','jitter_rap_2','jitter_rap_3','jitter_ppq5_mean','jitter_ppq5_1','jitter_ppq5_2','jitter_ppq5_3','Energy_mean','Energy_means001','Energy_means002','Energy_means003','shimmer_loc_mean','shimmer_loc_1','shimmer_loc_2','shimmer_loc_3','shimmer_local_dB_mean','shimmer_loc_db_1','shimmer_loc_db_2','shimmer_loc_db_3','shimmer_apq3_mean','shimmer_apq3_1','shimmer_apq3_2','shimmer_apq3_3','shimmer_apq5_mean','shimmer_apq5_1','shimmer_apq5_2','shimmer_apq5_3','shimmer_apq11_mean','shimmer_apq11_1','shimmer_apq11_2','shimmer_apq11_3','HNR05_mean','HNR05_means001','HNR05_means002','HNR05_means003','HNR15_mean','HNR15_means001','HNR15_means002','HNR15_means003','HNR25_mean','HNR25_means001','HNR25_means002','HNR25_means003','HNR35_mean','HNR35_means001','HNR35_means002','HNR35_means003','SHR_mean','SHR_means001','SHR_means002','SHR_means003','CPP_mean','CPP_means001','CPP_means002','CPP_means003','pF0_mean','pF0_means001','pF0_means002','pF0_means003','shrF0_mean','shrF0_means001','shrF0_means002','shrF0_means003','sF0_mean','sF0_means001','sF0_means002','sF0_means003','strF0_mean','strF0_means001','strF0_means002','strF0_means003','VoPT','sF1_mean','sF1_means001','sF1_means002','sF1_means003','vowel_dur','pre_is_voiced','fol_is_voiced','pre_is_obs','fol_is_obs','pre_exists','fol_exists','ms_from_utt_end','utt_per','ms_from_word_end','word_per','pre_is_voiced','fol_is_voiced','pre_is_obs','fol_is_obs','pre_exists','fol_exists','ms_from_utt_end','utt_per','ms_from_word_end','word_per']
+#features = ['H1A1c_mean','H1A1c_means001','H1A1c_means002','H1A1c_means003','H1A2c_mean','H1A2c_means001','H1A2c_means002','H1A2c_means003','H1A3c_mean','H1A3c_means001','H1A3c_means002','H1A3c_means003','H1H2c_mean','H1H2c_means001','H1H2c_means002','H1H2c_means003','H2H4c_mean','H2H4c_means001','H2H4c_means002','H2H4c_means003','H42Kc_mean','H42Kc_means001','H42Kc_means002','H42Kc_means003','H2KH5Kc_mean','H2KH5Kc_means001','H2KH5Kc_means002','H2KH5Kc_means003','jitter_loc_mean','jitter_loc_1','jitter_loc_2','jitter_loc_3','jitter_loc_abs_mean','jitter_loc_abs_1','jitter_loc_abs_2','jitter_loc_abs_3','jitter_rap_mean','jitter_rap_1','jitter_rap_2','jitter_rap_3','jitter_ppq5_mean','jitter_ppq5_1','jitter_ppq5_2','jitter_ppq5_3','Energy_mean','Energy_means001','Energy_means002','Energy_means003','shimmer_loc_mean','shimmer_loc_1','shimmer_loc_2','shimmer_loc_3','shimmer_local_dB_mean','shimmer_loc_db_1','shimmer_loc_db_2','shimmer_loc_db_3','shimmer_apq3_mean','shimmer_apq3_1','shimmer_apq3_2','shimmer_apq3_3','shimmer_apq5_mean','shimmer_apq5_1','shimmer_apq5_2','shimmer_apq5_3','shimmer_apq11_mean','shimmer_apq11_1','shimmer_apq11_2','shimmer_apq11_3','HNR05_mean','HNR05_means001','HNR05_means002','HNR05_means003','HNR15_mean','HNR15_means001','HNR15_means002','HNR15_means003','HNR25_mean','HNR25_means001','HNR25_means002','HNR25_means003','HNR35_mean','HNR35_means001','HNR35_means002','HNR35_means003','SHR_mean','SHR_means001','SHR_means002','SHR_means003','CPP_mean','CPP_means001','CPP_means002','CPP_means003','pF0_mean','pF0_means001','pF0_means002','pF0_means003','shrF0_mean','shrF0_means001','shrF0_means002','shrF0_means003','sF0_mean','sF0_means001','sF0_means002','sF0_means003','strF0_mean','strF0_means001','strF0_means002','strF0_means003','VoPT','sF1_mean','sF1_means001','sF1_means002','sF1_means003','vowel_dur','pre_is_voiced','fol_is_voiced','pre_is_obs','fol_is_obs','pre_exists','fol_exists','ms_from_utt_end','utt_per','ms_from_word_end','word_per','pre_is_voiced','fol_is_voiced','pre_is_obs','fol_is_obs','pre_exists','fol_exists','ms_from_utt_end','utt_per','ms_from_word_end','word_per']
 #features = ['H1A1c_mean','H1A1c_means001','jitter_loc_abs_2']
+features = ['H2H4c_mean']
 
 # Argument should be full path to file with data
 def parse_args():
@@ -51,10 +50,9 @@ def runClass(x, y):
     skf = StratifiedKFold(n_splits=5)
     x = x.as_matrix()
     y = np.array(y)
-    accuracyList = []
-    precList = []
-    recallList = []
-    fscoreList = []
+    y_pred_all = np.array([])
+    y_test_all = np.array([])
+    # Indented block happens within the fold
     for train_index, test_index in skf.split(x,y):
         x_train, x_test = x[train_index], x[test_index]
         y_train, y_test = y[train_index], y[test_index]
@@ -63,35 +61,19 @@ def runClass(x, y):
         x_test = pd.DataFrame(x_test)
         x_train = x_train.groupby(y_train).transform(fillNaOrZero)
         x_test = x_test.groupby(y_test).transform(fillNaOrZero)
+        # Fit classifier
         clf.fit(x_train, y_train)
-        acc = clf.score(x_test, y_test)
-        # NEW
-        acc = acc * 100
-        accuracyList.append(acc)
-        predictedy = cross_val_predict(clf, x_test, y_test)
-        #print(predictedy)
-        p, r, f, s = prfs(y_test, predictedy)
-        p = p * 100
-        r = r * 100
-        f = f * 100
-        precList.append(p)
-        recallList.append(r)
-        fscoreList.append(f)
-    acc_all.append(str(round(np.mean(accuracyList),3)))
-    prec_all.append(str(round(np.mean(precList),3)))
-    recall_all.append(str(round(np.mean(recallList),3)))
-    fscore_all.append(str(round(np.mean(fscoreList),3)))
+        y_pred = clf.predict(x_test)
+        y_pred_all = np.append(y_pred_all, y_pred)
+        y_test_all = np.append(y_test_all, y_test)
+    p, r, f, s = precision_recall_fscore_support(y_test_all, y_pred_all, average = 'weighted')
+    p = round((p * 100),3)
+    r = round((r * 100),3)
+    f = round((f * 100),3)
+    print(p, r, f)
+    acc = accuracy_score(y_test_all, y_pred_all)
+    print(round((acc * 100),3))
 
-# Calculates precision, recall, fscore, and support
-def prfs(trainy, predictedy):
-    y_true = np.array(trainy)
-    y_pred = np.array(predictedy)
-    return precision_recall_fscore_support(y_true, y_pred, average = 'weighted')
-
-acc_all = []
-prec_all = []
-recall_all = []
-fscore_all = []
 def main():
     args = parse_args()
     # Replace undefined and 0 with na
@@ -121,17 +103,6 @@ def main():
         runClass(x,y)
     #for i in udefcount:
     #    print(i)
-    #for i in acc_all:
-    #    print(i)
-    print("PRECISION")
-    for i in prec_all:
-        print(i)
-    print("RECALL")
-    for i in recall_all:
-        print(i)
-    print("FSCORE")
-    for i in fscore_all:
-        print(i)
 
 if __name__ == "__main__":
     main()
