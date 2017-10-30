7ab908790b1171998d02c84175ca814286beddd2
lpanfili <lpanfili@uw.edu>
2017-10-30 09:44:12 -0700

    Changed headers for consistency with other scripts

--- sortCorrelations.py
+++ sortCorrelations.py
@@ -17,24 +17,24 @@ def getPath(lg):
 	path = "/Users/Laura/Desktop/Dissertation/data/correlations/correlationMatrixNorm-" + lg + ".csv"
 	return path
 
-def getTop(data, type, lg):
+def sort(data, type, lg):
 	corr = pd.read_csv(data)
 	corr = corr.set_index('latex-feature')
 	corr = corr.round(decimals = 3)
-	corr = corr.rename(columns = {'breathy': 'B vs C', 'breathy.1': 'B vs M', 'creaky': 'C vs M'})
+	corr = corr.rename(columns = {'breathy': 'BC-corr', 'breathy.1': 'BM-corr', 'creaky': 'CM-corr'})
 	corr = corr[pd.notnull(corr.index)] # Remove breathy, creaky, and modal as features
 	# Add columns for absolute values
-	corr['BC-abs'] = corr['B vs C'].abs()
-	corr['BM-abs'] = corr['B vs M'].abs()
-	corr['CM-abs'] = corr['C vs M'].abs()
+	corr['BC-abs'] = corr['BC-corr'].abs()
+	corr['BM-abs'] = corr['BM-corr'].abs()
+	corr['CM-abs'] = corr['CM-corr'].abs()
 	# Sort each column by magnitude and pull out each comparison out as its own dataframe
 	corr = corr.sort_values(by = 'BC-abs', ascending = False)
-	BC  = corr[['B vs C']].copy().reset_index()
+	BC  = corr[['BC-corr']].copy().reset_index()
 	corr = corr.sort_values(by = 'BM-abs', ascending = False)
-	BM = corr[['B vs M']].copy().reset_index()
+	BM = corr[['BM-corr']].copy().reset_index()
 	corr = corr.sort_values(by = 'CM-abs', ascending = False)
-	CM = corr[['C vs M']].copy().reset_index()
-	# Put the three 
+	CM = corr[['CM-corr']].copy().reset_index()
+	# Put the three into one dataframe
 	sortedCorr = pd.concat([BC, BM, CM], axis = 1)
 	path = "/Users/Laura/Desktop/Dissertation/data/correlations/sortedCorr-" + lg + "-" + type + ".csv"
 	sortedCorr.to_csv(path)
@@ -43,8 +43,7 @@ def getTop(data, type, lg):
 def main():
 	args = parse_args()
 	path = getPath(args.lg)
-	# Change top to determine how many are printed
-	getTop(path, top, "imb", args.lg)
+	sort(path, "imb", args.lg)
 
 if __name__ == "__main__":
     main()
