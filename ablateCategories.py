c1da8c2f98c0ac282074d4292aadd383ca18f2c1
lpanfili <lpanfili@uw.edu>
2017-11-09 08:16:32 -0800

    First commit.

---
+++ ablateCategories.py
@@ -0,0 +1,263 @@
+# Runs an SVM and a RF excluding each category of features one at a time
+# Outputs a CSV for each classifier with the accuracy, precision, recall, and f-score for each cat
+
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import svm, metrics
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import StratifiedKFold
+import argparse
+from imblearn.over_sampling import SMOTE
+import csv
+import re
+
+# Arguments:
+# 1. CSV with features metadata
+# 2. 3-letter language code
+# 3. A comma-separated list of features to ablate
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('features_csv', type=str,
+                        help='a CSV file (with header) containing the features metadata')
+    parser.add_argument('lg', type=str, help='the three digit code for the language in question')
+    return parser.parse_args()
+
+
+# Makes a list of features based on the features metadata csv
+# Uses only features indicated with 1 in the csv, and features with <15% udef
+# Returns lists of features normalized in different ways (by speaker, overall, not at all)
+# Returns a dictionary of categories (key = cat, vals = feats in cat)
+def get_features(features_csv, lg):
+    features = []
+    by_speaker = [] # features to be normalized by speaker
+    no_normalize = [] # features that will not be normalized
+    to_normalize = [] # features to be normalized overall
+    cat_dict = {} # dictionary of each category of feature
+    with open(features_csv) as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for i in range(len(header)):
+            if header[i] == lg:
+                lgIndex = i
+        # Pick only features marked as 1 and with < 15% udef
+        for line in reader:
+            if line[lgIndex] == '1':
+                if float(line[lgIndex + 1]) < 15.0:
+                    features.append(line[3])
+                    if line[1] == '1':
+                        no_normalize.append(line[3])
+                    if line[2] == '1':
+                        by_speaker.append(line[3])
+                    if line[1] == "0" and line[2] == "0":
+                        to_normalize.append(line[3])
+                    cat = line[16]
+                    if cat not in cat_dict:
+                        cat_dict[cat] = []
+                    cat_dict[cat].append(line[3])
+    return features, by_speaker, no_normalize, to_normalize, cat_dict
+
+
+# Reads in a CSV of the data
+# Normalizes data
+# Returns x (features), y (labels), and the whole normalized data set
+def read_norm(lg, by_speaker, no_normalize, to_normalize, features, feature_dict):
+    csv = "../data/lgs/" + lg + "/" + lg + ".csv"
+    # Read CSV, replace undefined and 0 with NA
+    data = pd.read_csv(csv, na_values=["--undefined--",0])
+    # Replace feature names with latex-friendly names
+    feature_names = list(data)
+    for feat in feature_names:
+        if feat in feature_dict:
+            data = data.rename(columns = {feat: feature_dict[feat]})
+    zscore = lambda x: (x - x.mean())/x.std() # Define z-score
+    # Normalize some features by speaker
+    normalized_by_speaker = data[by_speaker+["speaker"]].groupby("speaker").transform(zscore)
+    # Normalize some features overall
+    normalized = zscore(data[to_normalize])
+    # The features that won't be normalized
+    notnormalized = data[no_normalize]
+    # List of phonation types
+    y = data['phonation'].tolist()
+    # Returns all the normalized (or not) data to one place
+    normalized = pd.concat([normalized, normalized_by_speaker, notnormalized], axis=1)
+    # REPLACE FEATURE NAMES IN NORMALIZED WITH LATEX FRIE
+    x = normalized[features]
+    return x, y, normalized
+
+
+# Replaces undefineds and zeros with the mean
+# If the mean is undefined, replaces with 0
+def fill_na_zero(x):
+    if not np.isnan(x.mean()): # If it's a number
+        return x.fillna(x.mean())
+    else: # If it's NaN
+        return x.fillna(0)
+
+
+# Runs SVM on resampled data set
+# Returns a list with the accuracy, precision, recall, and F-score
+def SVM_rs(x, y, features, lg):
+    clf = svm.SVC(kernel = 'linear')
+    skf = StratifiedKFold(n_splits=5)
+    sm = SMOTE(random_state=42)
+    x = x.as_matrix()
+    y = np.array(y)
+    y_pred_all = np.array([])
+    y_test_all = np.array([])
+    accuracyList = []
+    # Indented block happens within the fold
+    for train_index, test_index in skf.split(x,y):
+        x_train, x_test = x[train_index], x[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        # Replace undefined
+        x_train = pd.DataFrame(x_train)
+        x_test = pd.DataFrame(x_test)
+        x_train = x_train.groupby(y_train).transform(fill_na_zero)
+        x_test = x_test.groupby(y_test).transform(fill_na_zero)
+        # Resample (twice because there are three classes)
+        x_res, y_res = sm.fit_sample(x_train, y_train)
+        x_res, y_res = sm.fit_sample(x_res, y_res)
+        # Fit classifier
+        clf.fit(x_res, y_res)
+        y_pred = clf.predict(x_test)
+        y_pred_all = np.append(y_pred_all, y_pred)
+        y_test_all = np.append(y_test_all, y_test)
+    report = classification_report(y_test_all, y_pred_all)
+    prf = clean_report(report, lg)
+    acc = round((accuracy_score(y_test_all, y_pred_all)*100),3)
+    metrics = [str(round(acc,3))] + prf
+    return metrics
+
+
+# Runs RF on resampled data set
+# Returns a line with the accuracy, precision, recall, and F-score
+def RF_rs(x, y, features, lg):
+    clf = RandomForestClassifier()
+    skf = StratifiedKFold(n_splits=5)
+    sm = SMOTE(random_state=42)
+    x = x.as_matrix()
+    y = np.array(y)
+    y_pred_all = np.array([])
+    y_test_all = np.array([])
+    accuracyList = []
+    # Indented block happens within the fold
+    for train_index, test_index in skf.split(x,y):
+        x_train, x_test = x[train_index], x[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        # Replace undefined
+        x_train = pd.DataFrame(x_train)
+        x_test = pd.DataFrame(x_test)
+        x_train = x_train.groupby(y_train).transform(fill_na_zero)
+        x_test = x_test.groupby(y_test).transform(fill_na_zero)
+        # Resample (twice because there are three classes)
+        x_res, y_res = sm.fit_sample(x_train, y_train)
+        x_res, y_res = sm.fit_sample(x_res, y_res)
+        # Fit classifier
+        clf.fit(x_res, y_res)
+        y_pred = clf.predict(x_test)
+        y_pred_all = np.append(y_pred_all, y_pred)
+        y_test_all = np.append(y_test_all, y_test)
+    report = classification_report(y_test_all, y_pred_all)
+    prf = clean_report(report, lg)
+    acc = round((accuracy_score(y_test_all, y_pred_all)*100),3)
+    metrics = [str(round(acc,3))] + prf
+    return metrics
+
+
+# Takes in a classification report and the language code
+# Reorders and returns the report in an easier to manipulate format
+def clean_report(report, lg):
+    report_data = []
+    lines = report.split('\n')
+    if lg == 'cmn':
+        for line in lines[2:-3]:
+            row = []
+            row_data = re.split('       |      ', line)
+            row = row_data[1].strip(), float(row_data[2]), float(row_data[3]), float(row_data[4])
+            report_data.append(row)
+        cP = str(report_data[0][1])
+        cR = str(report_data[0][2])
+        cF = str(report_data[0][3])
+        mP = str(report_data[1][1])
+        mR = str(report_data[1][2])
+        mF = str(report_data[1][3])
+        prf = ["--", mP, cP, "--", mR, cR, "--", mF, cF]
+    elif lg == 'guj':
+        for line in lines[2:-3]:
+            row = []
+            row_data = re.split('       |      ', line)
+            row = row_data[1].strip(), float(row_data[2]), float(row_data[3]), float(row_data[4])
+            report_data.append(row)
+        bP = str(report_data[0][1])
+        bR = str(report_data[0][2])
+        bF = str(report_data[0][3])
+        mP = str(report_data[1][1])
+        mR = str(report_data[1][2])
+        mF = str(report_data[1][3])
+        prf = [bP, mP, "--", bR, mR, "--", bF, mF, "--"]
+    else:
+        for line in lines[2:-3]:
+            row = []
+            row_data = re.split('       |      ' ,line)
+            row = row_data[1].strip(), float(row_data[2]), float(row_data[3]), float(row_data[4])
+            report_data.append(row)
+        bP = str(report_data[0][1])
+        bR = str(report_data[0][2])
+        bF = str(report_data[0][3])
+        cP = str(report_data[1][1])
+        cR = str(report_data[1][2])
+        cF = str(report_data[1][3])
+        mP = str(report_data[2][1])
+        mR = str(report_data[2][2])
+        mF = str(report_data[2][3])
+        prf = [bP, mP, cP, bR, mR, cR, bF, mF, cF]
+    return prf
+
+
+# Makes a dictionary mapping the original feature names
+# To the LaTeX-friendly feature names
+def make_feature_dict(features_csv):
+    feature_dict = {}
+    with open(features_csv) as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for line in reader:
+            feature = line[0]
+            latex_feature = line[3]
+            if feature not in feature_dict:
+                feature_dict[feature] = ""
+            feature_dict[feature] = latex_feature
+    return feature_dict
+
+
+def main():
+    args = parse_args()
+    path = "../data/lgs/" + args.lg + "/" + args.lg
+    feature_dict = make_feature_dict(args.features_csv)
+    features, by_speaker, no_normalize, to_normalize, cat_dict = get_features(args.features_csv, args.lg)
+    x, y, data = read_norm(args.lg, by_speaker, no_normalize, to_normalize, features, feature_dict)
+    svm = []
+    rf = []
+    for cat in cat_dict:
+        x_abl = x
+        for feat in cat_dict[cat]:
+            x_abl = x_abl.drop([feat], axis = 1, inplace = False)
+        SVM_rs_aprf = SVM_rs(x_abl, y, features, args.lg)
+        SVM_rs_aprf.insert(0, cat)
+        SVM_rs_aprf.append(len(cat_dict[cat]))
+        RF_rs_aprf = RF_rs(x_abl, y, features, args.lg)
+        RF_rs_aprf.insert(0, cat)
+        RF_rs_aprf.append(len(cat_dict[cat]))
+        svm.append(SVM_rs_aprf)
+        rf.append(RF_rs_aprf)
+    svm = pd.DataFrame(svm, columns = ['cat', 'acc', 'pB', 'pM', 'pC', 'rB', 'rM', 'rC', 'fB', 'fM', 'fC', 'n feat'])
+    rf = pd.DataFrame(rf, columns = ['cat', 'acc', 'pB', 'pM', 'pC', 'rB', 'rM', 'rC', 'fB', 'fM', 'fC', 'n feat'])
+    svm.to_csv(path + "-abl-cat-SVM.csv")
+    rf.to_csv(path + "-abl-cat-RF.csv")
+
+
+if __name__ == "__main__":
+    main()
