b45106c722dddc177367b22ae0e6cfa22c7ebcaa
lpanfili <lpanfili@uw.edu>
2018-06-01 08:28:23 -0700

    Cleaning.

--- match/findHmong.py
+++
@@ -1,8 +0,0 @@
-# Finds all files that don't have a matching wave and textgrid
-
-import os
-import glob
-
-for root, dirs, files in os.walk("/Users/Laura/Desktop/"): 
-    for filename in files:
-        print(filename)
